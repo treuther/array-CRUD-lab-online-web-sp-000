dcfa414f486318381971ebdd9eff0a42933dc971
Tracy Reuther <tracyreuther@gmail.com>
2019-05-25 11:22:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  .last = remove_element_from_end_of_array.pop
+  -1 = remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
